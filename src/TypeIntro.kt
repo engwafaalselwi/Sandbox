9e396c28127e8a5cee10d9d0b80307725ac1575f
engwafaalselwi <69821038+engwafaalselwi@users.noreply.github.com>
2020-08-26 01:02:00 +0300

    Initial commit

---
+++ src/TypeIntro.kt
@@ -0,0 +1,22 @@
+
+    fun main(){
+        var experiencePoints:Int = 10;
+        println(experiencePoints);
+
+        var playerName = "Estragon";
+        println(playerName);
+
+        var magic = playerName.reversed()
+        println(magic)
+
+        var hasSteed = false
+        if(hasSteed == false){
+            println("They dont have dragon")
+
+        }else{
+            println("They have dragon")
+        }
+
+
+
+}
